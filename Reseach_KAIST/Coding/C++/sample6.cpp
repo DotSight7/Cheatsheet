f7d02f3af5b963cc6e7d9a5bd381ea3ac02cf48e
yocto <dbwogk7563@gmail.com>
2020-10-25 01:00:57 +0900

    Create sample6.cpp

---
+++ Reseach_KAIST/Coding/C++/sample6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Decimal 10->"<< 10<<"\n";
+    cout << "Hexadecimal 16->"<< 010 << "\n";
+    cout << "Octal 8->" << 0x10 << "\n";
+    cout << "Binary 2->" << 0xF <<"\n"; 
+    cout << "Octal Hex 6 : "<< 006 << ","<< 0x6 <<"\n";
+    cout << "Octal Hex 20 : "<< 024 << "," << 0x14 <<"\n";
+    cout << "Octal Hex 13 : "<< 015 << "," << 0xD << "\n";
+} // namespac
